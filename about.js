cd5c782ec8d1f34bf8eb7a7e139fd06e5df03585
Julian Wilkinson <jbwilkinson@ucdavis.edu>
2017-03-06 21:13:18 -0800

    Mobile and Desktop Skeleton; Color Meta

--- about.js
+++ about.js
@@ -3,10 +3,20 @@ $(document).ready(function(){
     // $(".hide").css("opacity", "0");
     $(".hide").toggleClass("hide-active");
     $(".projects").toggleClass("hide-active");
-    $(".navbar").toggleClass("hide-active");
-    $(".hide").on('transitionend',
+
+    $("#projects_button").toggleClass("hide-active");
+    $("#contact_button").toggleClass("hide-active");
+    // $(".navbar").toggleClass("hide-active");
+    $(".hide").one('transitionend',
     function () {
       $(".navbar a").css("cursor", "default");
+      $(".about").addClass("hide-active grow");
+      // $(".about").addClass("about_header_animx");
+      // $("#about_one.card").addClass("cardup");
+      // $(".about_header_animx").one('transitionend',
+      // function () {
+      //   $(".about_header_animx").addClass("about_header_animy");
+      // });
     });
 
     // $(".projects").css("opa city", "0");
