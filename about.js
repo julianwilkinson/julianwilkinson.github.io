660268b8ffb1320da374f5dea50249ffce3851d6
Julian Wilkinson <jbwilkinson@ucdavis.edu>
2017-06-17 13:47:01 -0700

    Removed Animations

--- about.js
+++ about.js
@@ -1,24 +1,24 @@
-$(document).ready(function(){
-  $( "#about_button" ).click(function(){
-    // $(".hide").css("opacity", "0");
-    $(".hide").toggleClass("hide-active");
-    $(".projects").toggleClass("hide-active");
-
-    $("#projects_button").toggleClass("hide-active");
-    $("#contact_button").toggleClass("hide-active");
-    // $(".navbar").toggleClass("hide-active");
-    $(".hide").one('transitionend',
-    function () {
-      $(".navbar a").css("cursor", "default");
-      $(".about").addClass("hide-active grow");
-      // $(".about").addClass("about_header_animx");
-      // $("#about_one.card").addClass("cardup");
-      // $(".about_header_animx").one('transitionend',
-      // function () {
-      //   $(".about_header_animx").addClass("about_header_animy");
-      // });
-    });
-
-    // $(".projects").css("opa city", "0");
-  });
-});
+// $(document).ready(function(){
+//   $( "#about_button" ).click(function(){
+//     // $(".hide").css("opacity", "0");
+//     $(".hide").toggleClass("hide-active");
+//     $(".projects").toggleClass("hide-active");
+//
+//     $("#projects_button").toggleClass("hide-active");
+//     $("#contact_button").toggleClass("hide-active");
+//     // $(".navbar").toggleClass("hide-active");
+//     $(".hide").one('transitionend',
+//     function () {
+//       $(".navbar a").css("cursor", "default");
+//       $(".about").addClass("hide-active grow");
+//       // $(".about").addClass("about_header_animx");
+//       // $("#about_one.card").addClass("cardup");
+//       // $(".about_header_animx").one('transitionend',
+//       // function () {
+//       //   $(".about_header_animx").addClass("about_header_animy");
+//       // });
+//     });
+//
+//     // $(".projects").css("opa city", "0");
+//   });
+// });
