7ff9e641235958ae6204526296a5181178be1fe8
Julian Wilkinson <jbwilkinson@ucdavis.edu>
2017-10-15 15:09:48 -0700

    Content Added, Projects Page
    Added About Me Basics,
    Added Safer Page Skeleton
    Fixed Bugs

--- scripts/main.js
+++ scripts/main.js
@@ -13,22 +13,52 @@ function cardSwitch(on_screen, off_screen, direction) {
 }
 
 function cardRoute(button) {
-	carousel_unit = $(button).parents(".carousel-unit")[0];
-	if (button.id.slice(0,4) === "back") {
-		off_screen = $("#about-card-main-carousel-unit")[0];
-		direction = "right";
-	}
-	else if (button.id === "academics-button") {
-		off_screen = $("#academics-card-carousel-unit")[0];
-		direction = "left";
-	}
-	else if (button.id === "volunteering-button") {
-		off_screen = $("#volunteering-card-carousel-unit")[0];
-		direction = "left";
-	}
+	animate(button, function() {
+		carousel_unit = $(button).parents(".carousel-unit")[0];
+		if (button.id.slice(0,4) === "back") {
+			off_screen = $("#about-card-main-carousel-unit")[0];
+			direction = "right";
+		}
+		else if (button.id === "academics-button") {
+			off_screen = $("#academics-card-carousel-unit")[0];
+			direction = "left";
+		}
+		else if (button.id === "volunteering-button") {
+			off_screen = $("#volunteering-card-carousel-unit")[0];
+			direction = "left";
+		}
 
-	console.log(carousel_unit);
-	console.log(off_screen);
-	cardSwitch(carousel_unit, off_screen, direction);
+
+		cardSwitch(carousel_unit, off_screen, direction);
+	});
 	return false;
 }
+
+
+function animate(obj, action) {
+	$(obj).addClass("animated");
+	$(obj).one('animationend', function() {
+		$(obj).removeClass("animated");
+		action();
+	});
+}
+
+function cardPeek() {
+	$("#academics-button-card").hover(cardPeekIn, cardPeekOut);
+	$("#volunteering-button-card").hover(cardPeekIn, cardPeekOut);
+}
+
+function cardPeekIn () {
+	console.log(this);
+	$("#academics-card-carousel-unit")[0].style.transform = "translateX(var(--card-right-side-peek-in))"
+
+}
+
+function cardPeekOut () {
+	$("#academics-card-carousel-unit")[0].style.transform = "translateX(var(--card-offscreen-right))"
+}
+
+// On Document Ready
+$(function() {
+	// cardPeek();
+})
